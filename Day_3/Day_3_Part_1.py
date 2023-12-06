75eb4933c36890722166bc8ec21f90eecfabfe16
quentin <quentin.winkel@agorinfo.fr>
2023-12-06 15:57:43 +0100

    Day_3_Part_1.py Progression

--- Day_3/Day_3_Part_1.py
+++ Day_3/Day_3_Part_1.py
@@ -9,15 +9,18 @@ data = """467..114..
 ...$.*....
 .664.598.."""
 
+list_lig = []
+
 for lig in data.split('\n'):
     list_nb = []
     nb = ""
     for character in lig:
         try:
+            int(character)
             nb = nb + character
         except ValueError:
             if nb != "":
                 list_nb.append(nb)
                 nb = ""
-
-    print(list_nb)
+    list_lig.append(list_nb)
+print(list_lig)
